2e175919513f76bb692a229515c7c33ce59fabfa
arsaizdihar <arsadihar@gmail.com>
2022-08-09 10:08:58 +0700

    Add color pallete

--- src/tailwind.config.cjs
+++ src/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-cream': '#FFF7E2',
+        'primary-pink': '#FEF5FF',
+        'primary-grey': '#D9D9D9',
+      },
+    },
   },
   plugins: [],
 };

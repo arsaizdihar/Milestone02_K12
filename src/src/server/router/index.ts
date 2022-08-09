041e15cf4e7754a7b37b5195bca5b940a6fcaed6
arsaizdihar <arsadihar@gmail.com>
2022-08-09 07:46:39 +0700

    Remove example router

--- src/src/server/router/index.ts
+++ src/src/server/router/index.ts
@@ -3,11 +3,9 @@ import superjson from 'superjson';
 import { createRouter } from './context';
 
 import { authRouter } from './auth';
-import { exampleRouter } from './example';
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge('example.', exampleRouter)
   .merge('auth.', authRouter);
 
 // export type definition of API

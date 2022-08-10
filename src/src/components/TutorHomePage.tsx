b33e00d9e25b70f129ca66dd1705459d1c726e42
arsaizdihar <arsadihar@gmail.com>
2022-08-10 12:20:14 +0700

    Add tutor's schedules frontend

--- src/src/components/TutorHomePage.tsx
+++ src/src/components/TutorHomePage.tsx
@@ -1,10 +1,22 @@
 import { useRouter } from 'next/router';
+import { trpc } from '~/utils/trpc';
 import Button from './Button';
 
 const TutorHomePage = () => {
   const router = useRouter();
+  const { data } = trpc.useQuery(['tutor.mySchedules']);
+
+  if (!data) return <div>Loading...</div>;
+
   return (
     <div>
+      <ul className="list-disc">
+        {data.map((schedule) => (
+          <li key={schedule.id}>
+            {schedule.subject} - {schedule.materi}
+          </li>
+        ))}
+      </ul>
       <Button onClick={() => router.push('/tutor/add-schedule')}>
         Add new schedule
       </Button>

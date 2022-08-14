38d9507519539d24c022db05cb07ea878f6b0bbd
WildanGhaly <16521502@std.stei.itb.ac.id>
2022-08-14 18:00:10 +0700

    ini cuman dicopas

--- src/src/server/router/course/student.ts
+++ src/src/server/router/course/student.ts
@@ -17,3 +17,48 @@ export const studentRouter = createStudentRouter().query('myCourses', {
     });
   },
 });
+
+
+import { TRPCError } from '@trpc/server';
+import bcrypt from 'bcryptjs';
+import signJWT from '.../signJWT';
+import { getUser, SIMPLE_USER_QUERY } from '.../user';
+import { createRouter } from '../context';
+
+export const authRouter = createRouter()
+  .mutation('editStudentProfile', {
+    input: z.object({
+      name: z.string(),
+      email: z
+        .string()
+        .trim()
+        .email()
+        .transform((email) => email.toLowerCase()),
+      password: z.string().min(8),
+      WANumber: z.string(),
+      lineId: z.string(),
+      photoUrl: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const ununiqueEmail = await ctx.prisma.user.findUnique({
+        where: { email: input.email },
+      });
+      if (ununiqueEmail) {
+        throw new TRPCError({
+          message: 'Email is already registered',
+          code: 'BAD_REQUEST',
+        });
+      }
+      const user = await ctx.prisma.user.create({
+        data: {
+          ...input,
+          password: bcrypt.hashSync(input.password, 10),
+          role: 'STUDENT',
+        },
+        select: SIMPLE_USER_QUERY,
+      });
+      signJWT(user, ctx.req, ctx.res);
+
+      return user;
+    },
+  })

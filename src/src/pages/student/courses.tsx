87f9ee1762b13c43a9130e44fe201f716975bbbe
NerbSerg <16521431@mahasiswa.itb.ac.id>
2022-08-15 19:17:56 +0700

    Change in student courses

--- src/src/pages/student/courses.tsx
+++ src/src/pages/student/courses.tsx
@@ -1,24 +1,34 @@
 import { useState } from 'react';
 import SearchField from '~/components/SearchField';
+import TimeSelect from '~/components/TimeSelect';
 import Title from '~/components/Title';
 import { useRedirect } from '~/hooks/useRedirect';
 import { trpc } from '~/utils/trpc';
+import Tabs from '~/components/Tabs';
 
-const TutorCoursesPage = () => {
+const TutorCoursesPage = () => { 
   const [search, setSearch] = useState('');
+  const [selectedTime, setSelectedTime] = useState('upcoming');
   useRedirect('STUDENT');
   return (
     <div className="flex flex-col gap-1">
-      <Title>YOUR COURSES</Title>
+      <Tabs
+        activeIndex={0}
+        tab1={{ label: 'Your Courses', href: '/student/courses' }}
+        tab2={{ label: 'Available', href: '/tutor' }}
+      />
+      {/* <Title>YOUR COURSES</Title> */}
       <SearchField onSearch={setSearch} />
-      <div>
+      <TimeSelect value={selectedTime as any} onChange={setSelectedTime} />
+      <Courses search={search} past={selectedTime === 'past'} />
+    <div>
         <h2 className="text-lg font-bold">Upcoming</h2>
         <Courses search={search} />
       </div>
-      <div>
+{/* /      <div>
         <h2 className="text-lg font-bold">Past</h2>
         <Courses past search={search} />
-      </div>
+      </div> */}
     </div>
   );
 };

13e4342ddae8802920d52fa2b648665e50c3743c
NerbSerg <16521431@mahasiswa.itb.ac.id>
2022-08-15 20:00:47 +0700

    Update courses.tsx

--- src/src/pages/student/courses.tsx
+++ src/src/pages/student/courses.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
 import SearchField from '~/components/SearchField';
 import TimeSelect from '~/components/TimeSelect';
-import Title from '~/components/Title';
 import { useRedirect } from '~/hooks/useRedirect';
 import { trpc } from '~/utils/trpc';
 import Tabs from '~/components/Tabs';
-import Button from '~/components/Button';
 
 const TutorCoursesPage = () => { 
   const [search, setSearch] = useState('');
@@ -18,15 +16,11 @@ const TutorCoursesPage = () => {
         tab1={{ label: 'Your Courses', href: '/student/courses' }}
         tab2={{ label: 'Available', href: '/student' }} 
       />
-      {/* <Title>YOUR COURSES</Title> */}
       <SearchField onSearch={setSearch} />
       <TimeSelect value={selectedTime as any} onChange={setSelectedTime} />
       <Courses search={search} past={selectedTime === 'past'} />
     <div>
-        {/* <h2 className="text-lg font-bold">Upcoming</h2>
-        <Courses search={search} /> */}
       </div>
-
     </div>
   );
 };

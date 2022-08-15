4ca0a59d17c6a27773c3bf65ad82ef00b1d5926a
rhmn5ty <rahman.satya@gmail.com>
2022-08-15 16:21:21 +0700

    Add fitur student take course

--- src/src/server/router/course/student.ts
+++ src/src/server/router/course/student.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { createStudentRouter } from '../context';
+import { TRPCError } from '@trpc/server';
 
 export const studentRouter = createStudentRouter()
   .query('myCourses', {
@@ -41,6 +42,32 @@ export const studentRouter = createStudentRouter()
           photoUrl: true,
           WANumber: true,
         },
+      });
+    },
+  })
+  .mutation('takeCourse', {
+    input: z.object({ courseId: z.string() }),
+    async resolve({ ctx, input }) {
+      const course = await ctx.prisma.course.findFirst({
+        where: { id: input.courseId },
+        include: { _count: { select: { participants: true } } },
+      });
+      if (course) {
+        if (course.slot === 0 || course.slot != course._count.participants) {
+          const newParticipant = await ctx.prisma.participation.create({
+            data: { userId: ctx.userId, courseId: input.courseId },
+          });
+          return newParticipant;
+        }
+
+        throw new TRPCError({
+          message: 'Course full',
+          code: 'BAD_REQUEST',
+        });
+      }
+      throw new TRPCError({
+        message: 'Course not found',
+        code: 'NOT_FOUND',
       });
     },
   });
